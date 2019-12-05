2f55b6508226ee783a90a2fab561ecb079bb399c
Amit Bothra <amit.bothra@outlook.com>
2019-12-04 19:43:58 -0600

    created model.py and moved model logic into it

---
+++ models.py
@@ -0,0 +1,60 @@
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+# we first create an SQLAchemy object and then configure the application later to support it
+db = SQLAlchemy()
+
+# Creating function to do following:
+# 1. connect app to local postgres database
+# 2. bind app to the SQLAlchemy 
+# # linking flask migrate to our app and database
+
+
+
+def bind_app_to_db(app):
+  app.config.from_object('config')
+  db.app = app
+  db.init_app(app)
+  migrate = Migrate(app, db)
+  return db
+
+#----------------------------------------------------------------------------#
+# Models.
+#----------------------------------------------------------------------------#
+    
+class Venue(db.Model):
+    __tablename__ = 'Venue'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    city = db.Column(db.String(120))
+    state = db.Column(db.String(120))
+    address = db.Column(db.String(120))
+    phone = db.Column(db.String(120))
+    image_link = db.Column(db.String(500))
+    facebook_link = db.Column(db.String(120))
+
+    def __repr__(self):
+      return f'<Venue {self.id} {self.name}>'
+
+    # TODO: implement any missing fields, as a database migration using Flask-Migrate
+
+class Artist(db.Model):
+    __tablename__ = 'Artist'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    city = db.Column(db.String(120))
+    state = db.Column(db.String(120))
+    phone = db.Column(db.String(120))
+    genres = db.Column(db.String(120))
+    image_link = db.Column(db.String(500))
+    facebook_link = db.Column(db.String(120))
+
+    def __repr__(self):
+      return f'<Artist {self.id} {self.name}>'
+
+
+    # TODO: implement any missing fields, as a database migration using Flask-Migrate
+
+# TODO Implement Show and Artist models, and complete all model relationships and properties, as a database migration.
