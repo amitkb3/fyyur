82932304f6a7d50310325ea7cfe1feb6f8d6f2e2
Amit Bothra <amit.bothra@outlook.com>
2019-12-25 18:54:54 -0600

    added Create New Artist endpoint

--- app.py
+++ app.py
@@ -446,14 +446,39 @@ def create_artist_form():
 
 @app.route('/artists/create', methods=['POST'])
 def create_artist_submission():
-  # called upon submitting the new artist listing form
-  # TODO: insert form data as a new Venue record in the db, instead
-  # TODO: modify data to be the data object returned from db insertion
+  
+  try:
+    form = ArtistForm()
+    name = form.name.data
+    city = form.city.data
+    state = form.state.data
+    phone = form.phone.data
+    genres = form.genres.data
+    facebook_link = form.facebook_link.data
+    website = form.website.data
+    image_link = form.image_link.data
+    if form.seeking_venue.data == 'Yes':
+      seeking_venue = True
+    else:
+      seeking_venue = False
+    seeking_description = form.seeking_description.data
 
-  # on successful db insert, flash success
-  flash('Artist ' + request.form['name'] + ' was successfully listed!')
-  # TODO: on unsuccessful db insert, flash an error instead.
-  # e.g., flash('An error occurred. Artist ' + data.name + ' could not be listed.')
+    artist = Artist(name=name, city=city,state=state,
+        phone=phone, genres=genres, 
+        facebook_link=facebook_link, website=website,
+        image_link=image_link, seeking_venue = seeking_venue,
+        seeking_description = seeking_description
+        )
+    db.session.add(artist)
+    db.session.commit()
+    # on successful db insert, flash success
+    flash('Artist ' + request.form['name'] + ' was successfully listed!')
+  except:
+    db.session.rollback()
+    flash('An error occured. Artist ' + request.form['name'] + ' could not be listed.')
+  finally:
+    db.session.close()
+
   return render_template('pages/home.html')
 
 
